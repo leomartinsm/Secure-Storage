c5644ad8d85b26c60fbbc1ffd18265acedbf3576
leonardo martins medeiros <leonardo.martins.medeiros@live.com>
2020-09-17 14:12:38 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./secure-storage.js",
     output: {
-        library: "SecureStorage",
+        library: "SecureStorageWithoutHashKey",
         path: './src/',
         filename: "secure-storage.js"
     }
